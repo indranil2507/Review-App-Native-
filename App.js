1daeb9a35809d3c7d67a7c4206314a86667d9bac
indranil2507 <iindranilsaha1@gmail.com>
2021-05-22 09:06:51 +0530

    fonts

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import Home from "./screens/home";
+import React, { useState } from 'react';
+import Home from './screens/home';
+import {useFonts} from 'expo-font';
+import { AppLoading } from 'expo-app-loading';
 
-export default function App() {
-  return <Home />;
+
+
+export default function App(){
+  
+
+  let [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-Semibold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+  });
+
+
+    return (
+     
+    <Home />
+    );
+  
+
 }
---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+  },
+  titleText: {
+    fontFamily: "nunito-Semibold",
+    fontSize: 24,
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
